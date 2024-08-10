34944ffb0e36fbfca9223c0ba2f8c5781ee4a335
Jamie Letts <jamieletts2210@gmail.com>
2024-08-10 08:34:00 +0100

    continue build on getGitHubUser user test

--- app/services/GitHubServices.scala
+++ app/services/GitHubServices.scala
@@ -7,7 +7,8 @@ import models.{APIError, DataModel}
 import org.bson.json.JsonObject
 import play.api.libs.json.{JsArray, JsObject, JsValue, Reads}
 
-import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.{LocalDate, ZonedDateTime}
 import java.util.Date
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
@@ -16,17 +17,22 @@ import scala.concurrent.{ExecutionContext, Future}
 class GitHubServices @Inject()(connector:GitHubConnector){
 
   def getGitHubUser(userName: String)(implicit ex: ExecutionContext): EitherT[Future, APIError, DataModel] = {
-   val url = "s\"https://api.github.com/users/$userName\""
+   val url = s"https://api.github.com/users/$userName"
 
     connector.get[JsValue](url)(Reads.JsValueReads, ex).subflatMap { json =>
       println("Here")
+
+      // Extract the created_at string from the JSON and convert it to LocalDate
+
+
       json.asOpt[JsObject] match {
+
         case Some(item) =>
           val userName: String = (item \ "login").as[String]
           val dateAccount = (item\ "created_at").as[LocalDate]
-          val location = (item \ "location").as[String]
-          val numberOfFollowers = (item\ "followers").as[Int]
-          val numberFollowing = (item \ "following").as[Int]
+          val location = (item \ "location").asOpt[String].getOrElse("")
+          val numberOfFollowers = (item\ "followers").asOpt[Int].getOrElse(0)
+          val numberFollowing = (item \ "following").asOpt[Int].getOrElse(0)
           val githubAccount = true
 
           Right(DataModel(userName = userName, dateAccount = dateAccount, location = location, numberOfFollowers = numberOfFollowers, numberFollowing = numberFollowing, gitHubAccount = githubAccount))
--- test/servicesSpec/GithubServicesSpec.scala
+++ test/servicesSpec/GithubServicesSpec.scala
@@ -2,72 +2,51 @@ package servicesSpec
 
 import cats.data.EitherT
 import connector.connectors.GitHubConnector
-import models. DataModel
+import models.{APIError, DataModel}
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
-import play.api.libs.json.{JsValue, Json, OFormat}
+import play.api.libs.json.{JsNull, JsValue, Json, OFormat}
 import services.GitHubServices
 
-import scala.concurrent.ExecutionContext
+import java.time.LocalDate
+import scala.concurrent.{ExecutionContext, Future}
 
 class GithubServicesSpec extends AnyWordSpec with MockFactory with ScalaFutures with GuiceOneAppPerSuite with Matchers {
 
   val mockConnector: GitHubConnector = mock[GitHubConnector]
   implicit val executionContext: ExecutionContext = app.injector.instanceOf[ExecutionContext]
   val testService = new GitHubServices(mockConnector)
-  val testData: JsValue = Json.parse(
-    """
-      |{
-      |  "login": "SpencerCGriffiths",
-      |  "id": 130156132,
-      |  "node_id": "U_kgDOB8IGZA",
-      |  "avatar_url": "https://avatars.githubusercontent.com/u/130156132?v=4",
-      |  "gravatar_id": "",
-      |  "url": "https://api.github.com/users/SpencerCGriffiths",
-      |  "html_url": "https://github.com/SpencerCGriffiths",
-      |  "followers_url": "https://api.github.com/users/SpencerCGriffiths/followers",
-      |  "following_url": "https://api.github.com/users/SpencerCGriffiths/following{/other_user}",
-      |  "gists_url": "https://api.github.com/users/SpencerCGriffiths/gists{/gist_id}",
-      |  "starred_url": "https://api.github.com/users/SpencerCGriffiths/starred{/owner}{/repo}",
-      |  "subscriptions_url": "https://api.github.com/users/SpencerCGriffiths/subscriptions",
-      |  "organizations_url": "https://api.github.com/users/SpencerCGriffiths/orgs",
-      |  "repos_url": "https://api.github.com/users/SpencerCGriffiths/repos",
-      |  "events_url": "https://api.github.com/users/SpencerCGriffiths/events{/privacy}",
-      |  "received_events_url": "https://api.github.com/users/SpencerCGriffiths/received_events",
-      |  "type": "User",
-      |  "site_admin": false,
-      |  "name": "Spencer Clarke-Griffiths",
-      |  "company": null,
-      |  "blog": "",
-      |  "location": null,
-      |  "email": null,
-      |  "hireable": null,
-      |  "bio": null,
-      |  "twitter_username": null,
-      |  "public_repos": 14,
-      |  "public_gists": 0,
-      |  "followers": 2,
-      |  "following": 2,
-      |  "created_at": "2023-04-07T12:50:03Z",
-      |  "updated_at": "2024-07-02T09:55:22Z"
-      |}
-""".stripMargin
+  val testData: JsValue = Json.obj(
+      "login" -> "SpencerCGriffiths",
+      "name" -> "Spencer Clarke-Griffiths",
+      "location" -> "",
+      "followers" -> 2,
+      "following" -> 2,
+      "created_at" -> "2023-04-07T12:50:03Z"
   )
 
-  "getGoogleBook" should {
+  "getGitHubUser" should {
     val url: String = "testUrl"
     "return a book" in {
       (mockConnector.get[DataModel](_: String)(_: OFormat[DataModel], _: ExecutionContext))
         .expects(url, *, *)
-        .returning(EitherT.rightT(testData))
+        .returning(EitherT.rightT[Future, APIError](testData.as[DataModel]))
         .once()
 
       whenReady(testService.getGitHubUser(userName = "SpencerCGriffiths").value) {
         result =>
-          result shouldBe EitherT.rightT(DataModel)
+          result shouldBe Right(DataModel(
+            userName = "SpencerCGriffiths",
+            dateAccount = LocalDate.parse("2023-04-07"),
+            location = "",
+            numberOfFollowers = 2,
+            numberFollowing = 2,
+            gitHubAccount = true
+          )
+          )
       }
     }
   }
