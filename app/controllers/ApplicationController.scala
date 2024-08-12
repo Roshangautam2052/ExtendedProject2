8ae44d47293d2c1d5b2fc4363a8010184df892d1
Roshan Gautam <roshangautam2052@gmail.com>
2024-08-12 15:05:18 +0100

    fixed logic for create user

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -2,29 +2,28 @@ package controllers
 
 import models.APIError
 import play.api.libs.json.Json
-import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
+import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents, Result}
 import services.{GitHubServices, RepositoryServices}
 
 import javax.inject._
 import scala.concurrent.duration.DurationInt
-import scala.concurrent.{Await, ExecutionContext}
+import scala.concurrent.{Await, ExecutionContext, Future}
 
 @Singleton
 class ApplicationController @Inject()(val controllerComponents: ControllerComponents,
                                       val service: GitHubServices, val repositoryServices: RepositoryServices)(implicit val ex: ExecutionContext) extends BaseController{
 
   def createUser(userName: String): Action[AnyContent] = Action.async { implicit request =>
-    service.getGitHubUser(userName).value.map {
+    service.getGitHubUser(userName).value.flatMap {
       case Right(dataModel) => {
         val response = repositoryServices.createUser(dataModel)
         response.map {
           case Right(createdUser) => Created(Json.toJson(createdUser))
-          case Left(APIError.BadAPIResponse(status, upstreamMessage)) => BadRequest(Json.toJson(upstreamMessage))
+          case Left(APIError.BadAPIResponse(status, upstreamMessage)) =>BadRequest(Json.toJson(upstreamMessage))
+          case Left(APIError.NotFoundError(status, upstreamMessage)) =>NotFound(Json.toJson(upstreamMessage))
         }
-        Await.result(response, 5.seconds)
-
       }
-      case Left(error) => Status(error.httpResponseStatus)
+      case Left(error) => Future.successful(Status(error.httpResponseStatus))
     }
   }
 
