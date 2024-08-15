a20e857dde2058829a04a091362fe635a86b6b10
Roshan Gautam <106965552+Roshangautam2052@users.noreply.github.com>
2024-08-15 10:15:22 +0100

    display user repos (#7)

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -130,7 +130,7 @@ class ApplicationController @Inject()(val controllerComponents: ControllerCompon
 
   def getGitHubRepos(userName: String): Action[AnyContent] = Action.async { implicit request =>
     gitService.getGitHubRepo(userName).value.map {
-      case Right(publicRepos) => Ok(Json.toJson(publicRepos))
+      case Right(publicRepos) => Ok(views.html.displayuserRepos(Some(publicRepos)))
       case Left(error) => Status(error.httpResponseStatus)
     }
   }
