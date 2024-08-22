b18caafe58012c48c97d3abf52d41460488a8bdc
Jamie Letts <jamieletts2210@gmail.com>
2024-08-22 10:31:22 +0100

    add styling and go back buttons

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,7 @@
 // Updates the copyright year in footer with the current year
 const year = document.querySelector('#current-year');
 year.innerHTML = new Date().getFullYear();
+
+document.querySelector(".goBack").addEventListener("click", function() {
+    window.history.go(-1);
+});
